b4fee039311b47b9be034f65a23e1b16355a84f2
Ivan Markin <mark-in@users.noreply.github.com>
2015-06-25 00:04:26 +0000

    2.0.11 (19-Aug-2006)

---
+++ wml_aux/iselect/config_ac.h
@@ -0,0 +1,36 @@
+/* config_ac.h.  Generated automatically by configure.  */
+/* config_ac.h.in.  Generated automatically from configure.in by autoheader.  */
+#ifndef CONFIG_AC_H
+#define CONFIG_AC_H
+/*
+**  config_ac.h -- AUTO configuration header file
+**  Copyright (c) Ralf S. Engelschall, <rse@engelschall.com>
+*/
+
+
+/* Define to empty if the keyword does not work.  */
+/* #undef const */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+#define USE_NCURSES 1 
+/* #undef USE_SLCURSES */ 
+/* #undef USE_CURSES */   
+
+/* Define if you have the <stddef.h> header file.  */
+#define HAVE_STDDEF_H 1
+
+/* Define if you have the <stdio.h> header file.  */
+#define HAVE_STDIO_H 1
+
+/* Define if you have the <stdlib.h> header file.  */
+#define HAVE_STDLIB_H 1
+
+/* Define if you have the <string.h> header file.  */
+#define HAVE_STRING_H 1
+
+/* Define if you have the <strings.h> header file.  */
+#define HAVE_STRINGS_H 1
+
+#endif /* CONFIG_AC_H */
